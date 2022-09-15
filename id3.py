273c1d7cbd87bb9ac4addf272160764825ba9bcb
JoaquinPettinari <joaquinpettinari@hotmail.com>
2022-09-14 23:56:56 -0300

    Adding id3

--- id3.py
+++ id3.py
@@ -1,90 +1,172 @@
 import pandas as pd
 import math
-import numpy as np
+from collections import Counter
 
-data = pd.read_csv("Test.csv")
-features = [feat for feat in data]
-features.remove("Creditability")
+df = pd.read_csv("Training6.csv")
+print("\n Given Play Golf Dataset:\n\n", df)
 
-class Node:
-    def __init__(self):
-        self.children = []
-        self.value = ""
-        self.isLeaf = False
-        self.pred = ""
+t = df.keys()[-1]
+print('Target Attribute is   ➡ ', t)
 
-def entropy(examples):
-    pos = 0.0
-    neg = 0.0
-    for _, row in examples.iterrows():
-        if row["Creditability"] == 1:
-            pos += 1
-        else:
-            neg += 1
-    if pos == 0.0 or neg == 0.0:
-        return 0.0
+# Get the attribute names from input dataset
+attribute_names = list(df.keys())
+
+#Remove the target attribute from the attribute names list
+attribute_names.remove(t) 
+
+print('Predicting Attributes ➡ ', attribute_names)
+#Function to calculate the entropy of probaility of observations
+# -p*log2*p
+
+def entropy(probs):  
+    return sum( [-prob*math.log(prob, 2) for prob in probs])
+
+#Function to calulate the entropy of the given Datasets/List with respect to target attributes
+def entropy_of_list(ls,value):  
+    
+    # Total intances associated with respective attribute
+    total_instances = len(ls)  # = 14
+    print("---------------------------------------------------------")
+    print("\nTotal no of instances/records associated with '{0}' is ➡ {1}".format(value,total_instances))
+    # Counter calculates the propotion of class
+    cnt = Counter(x for x in ls)
+    print('\nTarget attribute class count(Yes/No)=',dict(cnt))
+    
+    # x means no of YES/NO
+    probs = [x / total_instances for x in cnt.values()]  
+    print("\nClasses➡", max(cnt), min(cnt))
+    print("\nProbabilities of Class 'p'='{0}' ➡ {1}".format(max(cnt),max(probs)))
+    print("Probabilities of Class 'n'='{0}'  ➡ {1}".format(min(cnt),min(probs)))
+    
+    # Call Entropy 
+    return entropy(probs)
+
+def information_gain(df, split_attribute, target_attribute,battr):
+    print("\n\n----- Information Gain Calculation of",split_attribute,"----- ") 
+    
+    # group the data based on attribute values
+    df_split = df.groupby(split_attribute) 
+    glist=[]
+    for gname,group in df_split:
+        print('Grouped Attribute Values \n',group)
+        print("---------------------------------------------------------")
+        glist.append(gname) 
+    
+    glist.reverse()
+    nobs = len(df.index) * 1.0   
+    df_agg1=df_split.agg({target_attribute:lambda x:entropy_of_list(x, glist.pop())})
+    df_agg2=df_split.agg({target_attribute :lambda x:len(x)/nobs})
+    
+    df_agg1.columns=['Entropy']
+    df_agg2.columns=['Proportion']
+    
+    # Calculate Information Gain:
+    new_entropy = sum( df_agg1['Entropy'] * df_agg2['Proportion'])
+    if battr !='S':
+        old_entropy = entropy_of_list(df[target_attribute],'S-'+str(df.iloc[0][df.columns.get_loc(battr)]))
     else:
-        p = pos / (pos + neg)
-        n = neg / (pos + neg)
-        return -(p * math.log(p, 2) + n * math.log(n, 2))
+        old_entropy = entropy_of_list(df[target_attribute],battr)
+    return old_entropy - new_entropy
 
-def info_gain(examples, attr):
-    uniq = np.unique(examples[attr])
-    #print ("\n",uniq)
-    gain = entropy(examples)
-    #print ("\n",gain)
-    for u in uniq:
-        subdata = examples[examples[attr] == u]
-        #print ("\n",subdata)
-        sub_e = entropy(subdata)
-        gain -= (float(len(subdata)) / float(len(examples))) * sub_e
-        #print ("\n",gain)
-    return gain
+def id3(df, target_attribute, attribute_names, default_class=None,default_attr='S'):
+    
+    from collections import Counter
+    cnt = Counter(x for x in df[target_attribute])# class of YES /NO
+    
+    ## First check: Is this split of the dataset homogeneous?
+    if len(cnt) == 1:
+        return next(iter(cnt))  # next input data set, or raises StopIteration when EOF is hit.
+    
+    ## Second check: Is this split of the dataset empty? if yes, return a default value
+    elif df.empty or (not attribute_names):
+        return default_class  # Return None for Empty Data Set
+    
+    ## Otherwise: This dataset is ready to be devied up!
+    else:
+        # Get Default Value for next recursive call of this function:
+        default_class = max(cnt.keys()) #No of YES and NO Class
+        # Compute the Information Gain of the attributes:
+        gainz=[]
+        for attr in attribute_names:
+            ig= information_gain(df, attr, target_attribute,default_attr)
+            gainz.append(ig)
+            print('\nInformation gain of','“',attr,'”','is ➡', ig)
+            print("=========================================================")
+        
+        index_of_max = gainz.index(max(gainz))               # Index of Best Attribute
+        best_attr = attribute_names[index_of_max]            # Choose Best Attribute to split on
+        print("\nList of Gain for arrtibutes:",attribute_names,"\nare:", gainz,"respectively.")
+        print("\nAttribute with the maximum gain is ➡", best_attr)
+        print("\nHence, the Root node will be ➡", best_attr)
+        print("=========================================================")
 
-def ID3(examples, attrs):
-    root = Node()
+        # Create an empty tree, to be populated in a moment
+        tree = {best_attr:{}} # Initiate the tree with best attribute as a node 
+        remaining_attribute_names =[i for i in attribute_names if i != best_attr]
+        
+        # Split dataset-On each split, recursively call this algorithm.Populate the empty tree with subtrees, which
+        # are the result of the recursive call
+        for attr_val, data_subset in df.groupby(best_attr):
+            subtree = id3(data_subset,target_attribute, remaining_attribute_names,default_class,best_attr)
+            tree[best_attr][attr_val] = subtree
+        return tree
 
-    max_gain = 0
-    max_feat = ""
-    for feature in attrs:
-        #print ("\n",examples)
-        gain = info_gain(examples, feature)
-        if gain > max_gain:
-            max_gain = gain
-            max_feat = feature
-    root.value = max_feat
-    #print ("\nMax feature attr",max_feat)
-    uniq = np.unique(examples[max_feat])
-    #print ("\n",uniq)
-    for u in uniq:
-        #print ("\n",u)
-        subdata = examples[examples[max_feat] == u]
-        #print ("\n",subdata)
-        if entropy(subdata) == 0.0:
-            newNode = Node()
-            newNode.isLeaf = True
-            newNode.value = u
-            newNode.pred = np.unique(subdata["Creditability"])
-            root.children.append(newNode)
-        else:
-            dummyNode = Node()
-            dummyNode.value = u
-            new_attrs = attrs.copy()
-            new_attrs.remove(max_feat)
-            child = ID3(subdata, new_attrs)
-            dummyNode.children.append(child)
-            root.children.append(dummyNode)
-    return root
 
-def printTree(root: Node, depth=0):
-    for i in range(depth):
-        print("\t", end="")
-    print(root.value, end="")
-    if root.isLeaf:
-        print(" -> ", root.pred)
-    print()
-    for child in root.children:
-        printTree(child, depth + 1)
+#Function to calulate the entropy of the given Dataset with respect to target attributes
+def entropy_dataset(a_list):  
+    from collections import Counter
 
-root = ID3(data, features)
-printTree(root)
+    # Counter calculates the propotion of class
+    cnt = Counter(x for x in a_list)   
+    num_instances = len(a_list)*1.0    # = 14
+    print("\nNumber of Instances of the Current Sub-Class is {0}".format(num_instances ))
+    
+    # x means no of YES/NO
+    probs = [x / num_instances for x in cnt.values()]  
+    print("\nClasses➡", "'p'=",max(cnt), "'n'=",min(cnt))
+    print("\nProbabilities of Class 'p'='{0}' ➡ {1}".format(max(cnt),max(probs)))
+    print("Probabilities of Class 'n'='{0}'  ➡ {1}".format(min(cnt),min(probs)))
+    
+    # Call Entropy
+    return entropy(probs) 
+    
+# The initial entropy of the YES/NO attribute for our dataset.
+print("Entropy calculation for input dataset:\n")
+print(df['Creditability'])
+
+total_entropy = entropy_dataset(df['Creditability'])
+print("\nTotal Entropy(S) of Creditability Dataset➡", total_entropy)
+print("=========================================================")
+####################################################
+
+from pprint import pprint
+tree = id3(df,t,attribute_names)
+print("\nThe Resultant Decision Tree is: ⤵\n")
+pprint(tree)
+
+attribute = next(iter(tree))
+print("\nBest Attribute ➡",attribute)
+print("Tree Keys      ➡",tree[attribute].keys())
+
+# Converting categorical variables into dummies/indicator variables
+
+
+from sklearn.model_selection import train_test_split
+
+X = df.drop('Creditability',axis=1)
+y = df['Creditability']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=101)
+
+from sklearn.tree import DecisionTreeClassifier
+
+dtree = DecisionTreeClassifier(criterion='entropy',max_depth=3)
+dtree.fit(X_train,y_train)
+predictions = dtree.predict(X_test)
+
+from sklearn.tree import plot_tree
+import matplotlib.pyplot as plt
+
+fig = plt.figure(figsize=(16,12))
+a = plot_tree(dtree, feature_names=df.columns, fontsize=12, filled=True, 
+              class_names=['Not_Pay', 'Pay'])
+plt.show()
