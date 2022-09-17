0577b000c3f369c7628b13935ac37615d55ea131
JoaquinPettinari <joaquinpettinari@hotmail.com>
2022-09-16 21:14:24 -0300

    NOW IS WORKING THANNKS INDI FOR EVERYTHING

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import pandas as pd
-from set import Set
-from id3 import ID_3
+from id3 import DecisionTreeClassifier
 
-df = pd.read_csv("Test6.csv")
-#Nombre clase primaria
-clasePrimaria = df.keys()[-1]
+col_names = ["Account Balance","Payment Status of Previous Credit","Value Savings/Stocks","Length of current employment","Sex & Marital Status","Guarantors","Creditability"]
+data = pd.read_csv("TrainingCompleto.csv", skiprows=1, header=None, names=col_names)
+X = data.iloc[:, :-1].values
+Y = data.iloc[:, -1].values.reshape(-1,1)
 
-# Lista de otros atributos
-nombres_atributos = list(df.keys())
-nombres_atributos.remove(clasePrimaria) 
+classifier = DecisionTreeClassifier(col_names, min_samples_split=3, max_depth=3)
+classifier.fit(X,Y)
+prediccion_Y = classifier.predict(X)
+#classifier.print_tree()
 
-arbol = ID_3(df, clasePrimaria, nombres_atributos)
-arbol.imprimirArbol()
+actual_Y = data.iloc[:, -1].tolist()
+print(prediccion_Y)
+print(actual_Y)
+TP, FP, TN, FN = classifier.obtener_metricas(prediccion_Y, actual_Y)
+accuracy = classifier.obtener_accuracy(TP, FP, TN, FN)
+from sklearn.metrics import accuracy_score
+print(accuracy)
+print(accuracy_score(actual_Y, prediccion_Y))
 
-#Entropia de los valores 0,1 Creditability
-entropia_total = arbol.entropia_de_un_conjunto(df[clasePrimaria])
-#print(entropia_total)
--- nodo.py
+++ nodo.py
@@ -1,12 +1,13 @@
-class Nodo:
-
-	def __init__(self, valor):
-		self.valor = valor
-		self.hijos  = []
-
-	def agregar_hijo(self, hijo):
-		self.hijos.append(hijo)	
-	
-	def imprimirme(self):
-		print(self.valor+ '\n')
-		print(self.hijos, '\n')
+class Node():
+    def __init__(self, feature_index=None, threshold=None, left=None, right=None, info_gain=None, value=None):
+        ''' constructor ''' 
+        
+        # for decision node
+        self.feature_index = feature_index
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.info_gain = info_gain
+        
+        # for leaf node
+        self.value = value
