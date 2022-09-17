7c24aae645c999a2605e9b7e31c467ee8b9c1351
JoaquinPettinari <joaquinpettinari@hotmail.com>
2022-09-17 10:40:29 -0300

    Emprolijando main

--- main.py
+++ main.py
@@ -18,40 +18,57 @@ X_Test = TestData.iloc[:, :-1].values
 Y_Test = TestData.iloc[:, -1].values.reshape(-1,1)
 
 arbol_Training = DecisionTree(atributos, min_samples_split=3, max_depth=3)
-#arbol_Training.fit(X_Training,Y_Training)
-
+arbol_Training.fit(X_Training,Y_Training)
 actual_Training = TrainingData.iloc[:, -1].tolist()
-#prediccion_Training = arbol_Training.predict(X_Training)
+prediccion_Training = arbol_Training.predict(X_Training)
 
-#arbol_Test = DecisionTree(atributos, min_samples_split=3, max_depth=3)
-#arbol_Test.fit(X_Test,Y_Test)
-#prediccion_Test = arbol_Test.predict(X_Test)
 
-#TP_Training, FP_Training, TN_Training, FN_Training = obtener_metricas(prediccion_Training, actual_Training)
-#accuracy_Training = obtener_accuracy(TP_Training, FP_Training, TN_Training, FN_Training)
-#precision_Training = obtener_precision(TP_Training, FP_Training)
-
+arbol_Test = DecisionTree(atributos, min_samples_split=3, max_depth=3)
+arbol_Test.fit(X_Test,Y_Test)
 actual_Test = TestData.iloc[:, -1].tolist()
-"""
-TP_Test, FP_Test, TN_Test, FN_Test = obtener_metricas(prediccion_Test, actual_Test)
-accuracy_Test = obtener_accuracy(TP_Test, FP_Test, TN_Test, FN_Test)
-precision_Test = obtener_precision(TP_Test, FP_Test)
+prediccion_ID3_Test = arbol_Test.predict(X_Test)
 
+TP_ID3_Training, FP_ID3_Training, TN_ID3_Training, FN_ID3_Training = obtener_metricas(prediccion_Training, actual_Training)
+accuracy_ID3_Training = obtener_accuracy(TP_ID3_Training, FP_ID3_Training, TN_ID3_Training, FN_ID3_Training)
+precision_ID3_Training = obtener_precision(TP_ID3_Training, FP_ID3_Training)
+
+
+TP_ID3_Test, FP_ID3_Test, TN_ID3_Test, FN_ID3_Test = obtener_metricas(prediccion_ID3_Test, actual_Test)
+accuracy_ID3_Test = obtener_accuracy(TP_ID3_Test, FP_ID3_Test, TN_ID3_Test, FN_ID3_Test)
+precision_ID3_Test = obtener_precision(TP_ID3_Test, FP_ID3_Test)
+
+print("ID3")
 print("Training: ")
-print("Accuracy: ", accuracy_Training)
-print("Precision: ", precision_Training)
+print("Accuracy: ", accuracy_ID3_Training)
+print("Precision: ", precision_ID3_Training)
 
 print("Test: ")
-print("Accuracy: ", accuracy_Test)
-print("Precision: ", precision_Test)
-"""
+print("Accuracy: ", accuracy_ID3_Test)
+print("Precision: ", precision_ID3_Test)
+print("------------------- \n" )
 
-random_Forest = RandomForest(atributos, n_trees=6, max_depth=10)
-random_Forest.fit(X_Training,Y_Training)
 
-y_prediction = random_Forest.predict(X_Training)
-TP_Training, FP_Training, TN_Training, FN_Training = obtener_metricas(y_prediction, actual_Training)
-accuracy_Test = obtener_accuracy(TP_Training, FP_Training, TN_Training, FN_Training)
-precision_Test = obtener_precision(TP_Training, FP_Training)
-print(accuracy_Test)
-print(precision_Test)
+random_Forest_Training = RandomForest(atributos, n_trees=6, max_depth=10)
+random_Forest_Training.fit(X_Training,Y_Training)
+Y_RF_Prediction_Training = random_Forest_Training.predict(X_Training)
+
+TP_RF_Training, FP_RF_Training, TN_RF_Training, FN_RF_Training = obtener_metricas(Y_RF_Prediction_Training, actual_Training)
+accuracy_RF_Training = obtener_accuracy(TP_RF_Training, FP_RF_Training, TN_RF_Training, FN_RF_Training)
+precision_RF_Training = obtener_precision(TP_RF_Training, FP_RF_Training)
+
+random_Forest_Test = RandomForest(atributos, n_trees=6, max_depth=10)
+random_Forest_Test.fit(X_Test, Y_Test)
+Y_RF_Prediction_Test = random_Forest_Test.predict(X_Test)
+
+TP_RF_Test, FP_RF_Test, TN_RF_Test, FN_RF_Test = obtener_metricas(Y_RF_Prediction_Test, actual_Test)
+accuracy_RF_Test = obtener_accuracy(TP_RF_Test, FP_RF_Test, TN_RF_Test, FN_RF_Test)
+precision_RF_Test = obtener_precision(TP_RF_Test, FP_RF_Test)
+
+print("Forest Random")
+print("Training: ")
+print("Accuracy: ", accuracy_RF_Training)
+print("Precision: ",precision_RF_Training)
+
+print("Test: ")
+print("Accuracy: ", accuracy_RF_Test)
+print("Precision: ", precision_RF_Test)
