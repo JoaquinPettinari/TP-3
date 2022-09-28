31433067d7838a356f2c294000a707b58ef8cb13
JoaquinPettinari <joaquinpettinari@hotmail.com>
2022-09-27 22:55:48 -0300

    Comments

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import pandas as pd
 from arbol_decision import ID3, obtener_clase_predecida
-from random_forest import RandomForest_Predict, getPrediccionClasses, RandomForest_Train
+from random_forest import RandomForest_predecir, RF_obtener_clase_predecida, RandomForest_Train, bootstraping
 from utils import obtener_metricas, obtener_accuracy, obtener_precision
 from pprint import pprint
 
-Training_Data = pd.read_csv("TrainingCompleto.csv")
+Training_Data = pd.read_csv("Training6.csv")
 Test_Data = pd.read_csv("TestCompleto.csv")
 primaryClass = Training_Data.keys()[-1]
 
 ID3_Training_Tree = ID3(Training_Data,Training_Data,Training_Data.columns[:-1])
-ID3_Test_Tree = ID3(Test_Data,Test_Data,Test_Data.columns[:-1])
+#ID3_Test_Tree = ID3(Test_Data,Test_Data,Test_Data.columns[:-1])
 
 actual_Y_Training = Training_Data.iloc[:, -1].tolist()
 actual_Y_Test = Test_Data.iloc[:, -1].tolist()
 
 
+"""
 print("ID3 - Training: ")
 pprint(ID3_Training_Tree)
 prediccion_Y_Training = obtener_clase_predecida(Training_Data,ID3_Training_Tree)
+
 TP_ID3_Training, FP_ID3_Training, TN_ID3_Training, FN_ID3_Training = obtener_metricas(actual_Y_Training, prediccion_Y_Training)
 print("Matriz: ")
 print(TP_ID3_Training, FP_ID3_Training)
@@ -35,10 +37,11 @@ print("ACCURACY: ", obtener_accuracy(TP_ID3_Test, FP_ID3_Test, TN_ID3_Test, FN_I
 
 print("------------------------------------------------------------------------------------------------------------")
 print("Random Forest - Training")
-RF_Training = RandomForest_Train(Training_Data, 5)
-RF_Test = RandomForest_Train(Test_Data, 1)
-
-pred_Y_RF_Training = getPrediccionClasses(Training_Data, RF_Training, primaryClass)
+"""
+RF_Training = RandomForest_Train(Training_Data, 1)
+#RF_Test = RandomForest_Train(Test_Data, 1)
+pred_Y_RF_Training = RF_obtener_clase_predecida(Training_Data, RF_Training, primaryClass)
+"""
 TP_RF_TRAINING, FP_RF_TRAINING, TN_RF_TRAINING, FN_RF_TRAINING = obtener_metricas(actual_Y_Training, pred_Y_RF_Training)
 print("Matriz: ")
 print(TP_RF_TRAINING, FP_RF_TRAINING)
@@ -48,10 +51,11 @@ print("PRECISION: ", obtener_precision(TP_RF_TRAINING, FP_RF_TRAINING))
 
     
 print("Random Forest - Test")
-pred_Y_RF_Test = getPrediccionClasses(Test_Data, RF_Test, primaryClass)
+pred_Y_RF_Test = RF_obtener_clase_predecida(Test_Data, RF_Test, primaryClass)
 TP_RF_Test, FP_RF_Test, TN_RF_Test, FN_RF_Test = obtener_metricas(actual_Y_Test, pred_Y_RF_Test)
 print("Matriz: ")
 print(TP_RF_Test, FP_RF_Test)
 print(FN_RF_Test, TN_RF_Test)
 print("ACCURACY: ", obtener_accuracy(TP_RF_Test, FP_RF_Test, TN_RF_Test, FN_RF_Test))
 print("PRECISION: ", obtener_precision(TP_RF_Test, FP_RF_Test))
+"""
--- random_forest.py
+++ random_forest.py
@@ -1,66 +1,47 @@
 # File name: RandomForest.py
 
 import numpy as np
-import time
-from collections import Counter
 from arbol_decision import ID3, predecir
 import scipy.stats as sps
-
 
 def bootstraping(conjunto):
     for i in range(len(conjunto)):        
         for k in range(len(conjunto.keys()) - 1):
-            valores_unicos = np.unique(conjunto[conjunto.keys()[k]])
-            conjunto[conjunto.keys()[k]][i] = np.random.choice(valores_unicos)
+            atributo = conjunto.keys()[k]
+            valores_unicos = np.unique(conjunto[atributo])
+            conjunto[atributo][i] = np.random.choice(valores_unicos)
     return conjunto
 
-
-def RandomForest_Train(dataset,number_of_Trees):
-    #Create a list in which the single forests are stored
-    random_forest_sub_tree = []
+def RandomForest_Train(dataset,numbero_de_arboles):
+    # Lista para ir guardando cada árbol
+    arboles_random_forest = []
     
-    #Create a number of n models
-    for i in range(number_of_Trees):
+    # For para la cantidad de arboles a crear
+    for i in range(numbero_de_arboles):
         # Crea los datos aleatorios del bootstraping
-        #bootstrap_sample = dataset.sample(frac=1,replace=True)
-        bootstrap_sample = bootstraping(dataset)
+        # De esta forma usa los mismos datos pero mezclados
+        #conjunto_bootstrap = dataset.sample(frac=1,replace=True)
+        # Con esta función crea random 100% dentro de los valores de la columna
+        conjunto_bootstrap = bootstraping(dataset)
+        # Se agrega a la lista el arbol que se crea con el conjunto de datos
+        arboles_random_forest.append(ID3(conjunto_bootstrap,conjunto_bootstrap,dataset.drop(labels=['Creditability'],axis=1).columns))
+    
+    return arboles_random_forest
 
-        print("-----------")
-        print(bootstrap_sample)
-        print("-----------")
-        
-        #Create a training and a testing datset by calling the train_test_split function
-        #bootstrap_training_data = train_test_split(bootstrap_sample)[0]
-        #bootstrap_testing_data = train_test_split(bootstrap_sample)[1] 
-        
-        #Grow a tree model for each of the training data
-        #We implement the subspace sampling in the ID3 algorithm itself. Hence take a look at the ID3 algorithm above!
-        random_forest_sub_tree.append(ID3(bootstrap_sample,bootstrap_sample,dataset.drop(labels=['Creditability'],axis=1).columns))
-        
-    return random_forest_sub_tree
-
-
-        
- 
-
-#######Predict a new query instance###########
-def RandomForest_Predict(query,random_forest,default='p'):
+def RandomForest_predecir(query,random_forest,default='p'):
     predictions = []
     for tree in random_forest:
         predictions.append(predecir(query,tree,default))
+    print(predictions)
+    print(sps.mode(predictions, keepdims=True))
     return sps.mode(predictions, keepdims=True)[0][0]
 
-
-
-
-
-#######Test the model on the testing data and return the accuracy###########
-def getPrediccionClasses(data,random_forest, primaryClass):
-    predictions = []
-    for i in range(len(data)):
-        query = data.iloc[i,:].drop(primaryClass).to_dict()
-        predictions.append(RandomForest_Predict(query,random_forest,default='p'))
+def RF_obtener_clase_predecida(conjunto,random_forest, clase_primaria):
+    predicciones = []
+    for i in range(len(conjunto)):
+        query = conjunto.iloc[i,:].drop(clase_primaria).to_dict()
+        predicciones.append(RandomForest_predecir(query,random_forest,default='p'))
     #accuracy = sum(predictions == data['Creditability'])/len(data)*100
     #print('The prediction accuracy is: ',sum(data['predictions'] == data['target'])/len(data)*100,'%')
-    return predictions
+    return predicciones
         
