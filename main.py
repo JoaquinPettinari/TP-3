defc8fe0951fe8347e948399a391f8f2b6f88cc1
JoaquinPettinari <joaquinpettinari@hotmail.com>
2022-10-13 20:55:49 -0300

    Agregando los pasos que dice el profe

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from perceptron import obtener_valores_de_columna, perceptron
-from sklearn import datasets
+from r2 import obtener_puntos, obtener_valores_de_columna, perceptron, plot_decision_boundary, trazar_linea
 import matplotlib.pyplot as plt
 from pandas import DataFrame
-import numpy as np
 
+"""
 TP3_1_X = [
     [10.02488611,  5.08333171],
     [ 6.86675981,  5.96880721],
@@ -26,35 +25,12 @@ TP3_1_X = [
     [ 0.23053498,  7.05967085],
     [ 7.32133123,  4.70066552],
 ]
-TP3_1_Y = [-1, -1, 1, -1, 1, -1, 1, 1, -1, 1, -1, -1, 1, 1, -1, 1, -1, 1, 1, -1]
+"""
 
-def plot_decision_boundary(TP_X, theta):
-    X_0 = obtener_valores_de_columna(0, TP_X)
-
-    x1 = [min(X_0), max(X_0)]
-    m = -theta[1]/theta[2]
-    print(m)    
-    c = -theta[0]/theta[2]
-    print(c)
-    x2 = m*x1 + c
-    
-    plt.xlabel("x1")
-    plt.ylabel("x2")
-    plt.xlim([0,10])
-    plt.ylim([0,10])
-    print(x1)
-    print(x2)
-    plt.plot(x1, x2)
-    plt.show()
-
-df = DataFrame(dict(x1=obtener_valores_de_columna(0, TP3_1_X), x2=obtener_valores_de_columna(1, TP3_1_X), y=TP3_1_Y))
-colores = {-1:'red', 1:'blue'}
-_, subplot = plt.subplots()
-#print(TP3_1_X)
-# Agrupa por los valores de Y
-valores_agrupados = df.groupby('y')
-for valor_Y, grupo in valores_agrupados:
-    grupo.plot(ax=subplot, x='x1', y='x2', label=valor_Y, color=colores[valor_Y], kind='scatter')
-
+trazar_linea()
+puntos = obtener_puntos()
+print(puntos)
+"""
 theta = perceptron(TP3_1_X, TP3_1_Y, 0.5)
 plot_decision_boundary(TP3_1_X, theta)
+"""
--- r2.py
+++ r2.py
@@ -1,4 +1,36 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+def trazar_linea():
+    plt.plot([0,8], [2,10])
+
+def formula_y(x):
+    print(x)
+    return x + 2
+
+def obtener_puntos():
+    puntos = np.empty((20,3))
+    for i in range(20):
+        for j in range(3):
+            if(j == 2):
+                x1 = puntos[i,0]
+                x2 = puntos[i,1]
+                puntos[i,j] = 1 if x2 <= formula_y(x1)  else -1
+            else:
+                puntos[i,j] = random.uniform(0, 10)
+    return puntos
+
+
+
+
+
+
+
+
+
+
+
 
 def step_func(z):
     return 1.0 if (z > 0) else -1.0
@@ -48,3 +80,20 @@ def perceptron(TP_X, y, lr):
         # at every iteration.
     print("-----------------")
     return theta
+
+def plot_decision_boundary(TP_X, theta):
+    X_0 = obtener_valores_de_columna(0, TP_X)
+
+    x1 = [min(X_0), max(X_0)]
+    m = -theta[1]/theta[2]
+    print(m)    
+    c = -theta[0]/theta[2]
+    print(c)
+    x2 = m*x1 + c
+    
+    plt.xlabel("x1")
+    plt.ylabel("x2")
+    plt.xlim([0,10])
+    plt.ylim([0,10])
+    plt.plot(x1, x2)
+    plt.show()
