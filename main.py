4c2cf5e12a91c83ac9bed7f98da69e288036c5cf
JoaquinPettinari <joaquinpettinari@hotmail.com>
2022-10-13 19:00:42 -0300

    ;:

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import numpy as np
 TP3_1_X = [
     [10.02488611,  5.08333171],
     [ 6.86675981,  5.96880721],
-    [ 0.23989814,  9.28353092],
+    [ 2.23989814,  4.28353092],
     [ 7.46134859,  4.58877252],
     [ 1.57845107,  8.26537513],
     [ 7.90593303,  4.20733751],
@@ -24,34 +24,37 @@ TP3_1_X = [
     [ 7.35736969,  5.13258805],
     [ 0.47871581,  9.33930476],
     [ 0.23053498,  7.05967085],
-    [ 7.63415083,  4.70066552],
+    [ 7.32133123,  4.70066552],
 ]
 TP3_1_Y = [-1, -1, 1, -1, 1, -1, 1, 1, -1, 1, -1, -1, 1, 1, -1, 1, -1, 1, 1, -1]
 
 def plot_decision_boundary(TP_X, theta):
     X_0 = obtener_valores_de_columna(0, TP_X)
-    # The Line is y=mx+c
-    # So, Equate mx+c = theta0.X0 + theta1.X1 + theta2.X2
-    # Solving we find m and c
+
     x1 = [min(X_0), max(X_0)]
     m = -theta[1]/theta[2]
+    print(m)    
     c = -theta[0]/theta[2]
+    print(c)
     x2 = m*x1 + c
     
     plt.xlabel("x1")
     plt.ylabel("x2")
-    #plt.plot(x1, x2)
+    plt.xlim([0,10])
+    plt.ylim([0,10])
+    print(x1)
+    print(x2)
+    plt.plot(x1, x2)
     plt.show()
 
 df = DataFrame(dict(x1=obtener_valores_de_columna(0, TP3_1_X), x2=obtener_valores_de_columna(1, TP3_1_X), y=TP3_1_Y))
 colores = {-1:'red', 1:'blue'}
-_, ax = plt.subplots()
+_, subplot = plt.subplots()
 #print(TP3_1_X)
 # Agrupa por los valores de Y
 valores_agrupados = df.groupby('y')
 for valor_Y, grupo in valores_agrupados:
-    grupo.plot(ax=ax, x='x1', y='x2', label=valor_Y, color=colores[valor_Y], kind='scatter')
+    grupo.plot(ax=subplot, x='x1', y='x2', label=valor_Y, color=colores[valor_Y], kind='scatter')
 
-theta, miss_l = perceptron(TP3_1_X, TP3_1_Y, 0.5, 100)
-print(theta)
+theta = perceptron(TP3_1_X, TP3_1_Y, 0.5)
 plot_decision_boundary(TP3_1_X, theta)
--- perceptron.py
+++ perceptron.py
@@ -1,55 +1,50 @@
 import numpy as np
 
 def step_func(z):
-    return 1.0 if (z > 0) else 0.0
+    return 1.0 if (z > 0) else -1.0
 
 def obtener_valores_de_columna(columna, matriz):
     return [i[columna] for i in matriz]
 
-def perceptron(TP_X, y, lr, epochs):
-    print(TP_X, y, lr, epochs)
-    # X --> Inputs.
-    # y --> labels/target.
-    # lr --> learning rate.
-    # epochs --> Number of iterations.
-    
-    # m-> number of training examples
+def perceptron(TP_X, y, lr):        
     X = np.matrix(TP_X)
-    # n-> number of features 
-    m, n = X.shape
+    # n-> número de atributos
+    _, n = X.shape
     
     # Initializing parapeters(theta) to zeros.
     # +1 in n+1 for the bias term.
     theta = np.zeros((n+1,1))
+    print(X)
     
     # Empty list to store how many examples were 
     # misclassified at every iteration.
-    n_miss_list = []
     
-    # Training.
-    for epoch in range(epochs):
+    # looping for every example.
+    for idx, x_i in enumerate(X):
         
-        # variable to store #misclassified.
-        n_miss = 0
-        
-        # looping for every example.
-        for idx, x_i in enumerate(X):
+        # Insering 1 for bias, X0 = 1.
+        print(x_i)
+        print(idx)
+        x_i = np.insert(x_i, 0, 1).reshape(-1,1)
+        print(x_i.T)
+        print(theta)
+        """
+        a = np.array([[1,2],[3,4]]) 
+        b = np.array([[11,12],[13,14]]) 
+        np.dot(a,b) -> [[1*11+2*13, 1*12+2*14],[3*11+4*13, 3*12+4*14]]
+
+        .T = [[1,2,3]] -> [[1][2][3]]
+        """
+        # Calculating prediction/hypothesis.
+        y_hat = step_func(np.dot(x_i.T, theta))
+        print(np.dot(x_i.T, theta))
+        # Updating if the example is misclassified.
+        if (np.squeeze(y_hat) - y[idx]) != 0:
+            theta += lr*((y[idx] - y_hat)*x_i)
             
-            # Insering 1 for bias, X0 = 1.
-            x_i = np.insert(x_i, 0, 1).reshape(-1,1)
-            
-            # Calculating prediction/hypothesis.
-            y_hat = step_func(np.dot(x_i.T, theta))
-            
-            # Updating if the example is misclassified.
-            if (np.squeeze(y_hat) - y[idx]) != 0:
-                theta += lr*((y[idx] - y_hat)*x_i)
-                
-                # Incrementing by 1.
-                n_miss += 1
-        
+            # Incrementing by 1.
+    
         # Appending number of misclassified examples
         # at every iteration.
-        n_miss_list.append(n_miss)
-        
-    return theta, n_miss_list
+    print("-----------------")
+    return theta
